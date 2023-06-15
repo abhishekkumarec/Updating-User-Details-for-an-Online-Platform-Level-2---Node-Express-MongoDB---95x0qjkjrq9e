e13d828d4f0ad682caaffafab85fca6adf24b5f5
Abhishek Kumar <126902053+abhishekkumarec@users.noreply.github.com>
2023-06-15 18:25:10 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,68 +1,36 @@
-const fs = require("fs");
-const express = require("express");
+const express = require('express');
+const bodyParser = require('body-parser');
+const userDetails = require('./data/userDetails.json');
+
 const app = express();
+app.use(bodyParser.json());
 
-// Importing products from userDetails.json file
-const userDetails = JSON.parse(
-  fs.readFileSync(`${__dirname}/data/userDetails.json`)
-);
+// PATCH request to update user details
+app.patch('/api/v1/details/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const index = userDetails.findIndex(user => user.id === id);
 
-//Middlewares
-app.use(express.json());
-
-// Write PATCH endpoint for editing user details
-
-// POST endpoint for registering new user
-app.post("/api/v1/details", (req, res) => {
-  const newId = userDetails[userDetails.length - 1].id + 1;
-  const { name, mail, number } = req.body;
-  const newUser = { id: newId, name, mail, number };
-  userDetails.push(newUser);
-  fs.writeFile(
-    `${__dirname}/data/userDetails.json`,
-    JSON.stringify(userDetails),
-    (err) => {
-      res.status(201).json({
-        status: "Success",
-        message: "User registered successfully",
-        data: {
-          userDetails: newUser,
-        },
-      });
-    }
-  );
-});
-
-// GET endpoint for sending the details of users
-app.get("/api/v1/details", (req, res) => {
-  res.status(200).json({
-    status: "Success",
-    message: "Detail of users fetched successfully",
-    data: {
-      userDetails,
-    },
-  });
-});
-
-// GET endpoint for sending the details of users by id
-app.get("/api/v1/userdetails/:id", (req, res) => {
-  let { id } = req.params;
-  id *= 1;
-  const details = userDetails.find((details) => details.id === id);
-  if (!details) {
-    return res.status(404).send({
-      status: "failed",
-      message: "User not found!",
-    });
+  if (index === -1) {
+    // User not found
+    res.status(404).json({ status: 'failed', message: 'User not found!' });
   } else {
-    res.status(200).send({
-      status: "success",
-      message: "Details of users fetched successfully",
-      data: {
-        details,
-      },
+    // Update user details
+    userDetails[index].name = req.body.name || userDetails[index].name;
+    userDetails[index].mail = req.body.mail || userDetails[index].mail;
+    userDetails[index].number = req.body.number || userDetails[index].number;
+
+    // Return updated user details
+    res.json({
+      status: 'success',
+      message: `User details updated successfully for id: ${id}`,
+      product: userDetails[index]
     });
   }
 });
 
-module.exports = app;
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+module.exports = app;
